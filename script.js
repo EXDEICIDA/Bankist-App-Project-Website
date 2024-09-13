e88104a98db2b1bcdc2aea3f97a668cd97da56a6
EXDEICIDA <javadsoltanov@gmail.com>
2024-09-13 16:24:35 +0300

    Menu fade animation

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const nav = document.querySelector('.nav');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -86,6 +87,32 @@ tabsContainer.addEventListener('click', function (e) {
 });
 
 //Menu fade animation for the links
+nav.addEventListener('mouseover', function (e) {
+  // Event function
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = 0.5; // Fixed typo
+    });
+    logo.style.opacity = 0.5;
+  }
+});
+
+nav.addEventListener('mouseout', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = 1; // Fixed typo
+    });
+    logo.style.opacity = 1;
+  }
+});
 
 //creating and inserting elements
 // .insertAdjacentHTML
